0d01bced0987a6af32268dd578dec63522bc9443
altairjr <altair-pereiralol@hotmail.com>
2021-07-06 12:36:52 -0300

    Concluido a tela de settings e adicionado musica
    Acrescentado funções da ui, tela de configurações está pronta. Adicionado musica de menu inicial no jogo.

--- Abduction/Abduction/Assets/Scripts/GameManager/GameController.cs
+++ Abduction/Abduction/Assets/Scripts/GameManager/GameController.cs
@@ -4,5 +4,59 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Audio Clip In Menu")]
+    [SerializeField] private AudioClip soundClip_;
 
+    [Header("Audio Clip In Game")]
+    [SerializeField] private AudioClip musicsClips_;
+
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource soundSource_;
+    [SerializeField] private AudioSource musicSource_;
+
+    public static bool soundON_ = true;
+    public static bool musicON_ = true;
+
+    private void Awake()
+    {
+        StartAudioGame();
+    }
+
+    private void Update()
+    {
+        PlayMusic();
+    }
+
+    #region System Audio
+
+    private void StartAudioGame()
+    {
+        soundON_ = true;
+        musicON_ = true;
+    }
+
+    private void PlayMusic()
+    {
+        if (musicON_)
+        {
+            musicSource_.clip = musicsClips_;
+            if (!musicSource_.isPlaying)
+                musicSource_.Play();
+        }
+        else
+        {
+            musicSource_.Stop();
+        }
+    }
+
+    public void OnClickButtonPlaySound()
+    {
+        if (soundON_)
+        {
+            soundSource_.clip = soundClip_;
+            soundSource_.Play();
+        }
+    }
+
+    #endregion
 }
--- Abduction/Abduction/Assets/Scripts/GameManager/MenuController.cs
+++ Abduction/Abduction/Assets/Scripts/GameManager/MenuController.cs
@@ -1,47 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [Header("Menu")]
     [SerializeField] private GameObject startMenu_;
     [SerializeField] private GameObject settingtMenu_;
-
-    [Header("Audio Clip In Menu")]
-    [SerializeField] private AudioClip audioClip_;
-
-    [Header("Audio Source")]
-    [SerializeField] private AudioSource audioSource_;
+    private GameObject menuActive_;
 
     [Header("Language EN")]
-    [SerializeField] private GameObject language_en_menuSettings_;
-    [SerializeField] private GameObject language_en_menuStart_;
+    [SerializeField] private GameObject[] language_en_;
     [SerializeField] private GameObject buttonActiveEN_;
     private bool selectEN_;
 
     [Header("Language PT")]
-    [SerializeField] private GameObject language_pt_menuSettings_;
-    [SerializeField] private GameObject language_pt_menuStart_;
+    [SerializeField] private GameObject[] language_pt_;
     [SerializeField] private GameObject buttonActivePT_;
     private bool selectPT_;
 
-    private GameObject menuActive_;
+    [Header("Sound On and OFF and Music")]
+    [SerializeField] private GameObject buttonSoundON_;
+    [SerializeField] private GameObject buttonSoundOFF_;
+    [Space]
+    [SerializeField] private GameObject buttonMusicON_;
+    [SerializeField] private GameObject buttonMusicOFF_;
 
     public static MenuController Instancia { get; set; }
 
     private void Awake()
     {
         ChangeMenu(startMenu_);;
-        ChangeButton(buttonActiveEN_);
-        ChangeLanguageSettings(language_en_menuSettings_);
-        ChangeLanguageStart(language_en_menuStart_);
+        CLanguagehangeButton(buttonActiveEN_);
     }
 
     private void Update()
     {
         SetLanguage();
+        SetButttonAudio();
     }
+
+    #region Menu Controller
 
     private void ChangeMenu(GameObject menu)
     {
@@ -51,24 +51,98 @@ public class MenuController : MonoBehaviour
         menu.SetActive(true);
     }
 
+    public void OnClickYesQuitGame()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    #region Menu Controller Audio
+
+    private void SetButttonAudio()
+    {
+        if (GameController.soundON_)
+        {
+            ChangeButtonSound(buttonSoundON_);
+        }
+        else
+        {
+            ChangeButtonSound(buttonSoundOFF_);
+        }
+
+        if (GameController.musicON_)
+        {
+            ChangeButtonMusic(buttonMusicON_);
+        }
+        else
+        {
+            ChangeButtonMusic(buttonMusicOFF_);
+        }
+    }
+
+    private void ChangeButtonSound(GameObject sound)
+    {
+        buttonSoundON_.SetActive(false);
+        buttonSoundOFF_.SetActive(false);
+
+        sound.SetActive(true);
+    }
+
+    private void ChangeButtonMusic(GameObject music)
+    {
+        buttonMusicON_.SetActive(false);
+        buttonMusicOFF_.SetActive(false);
+
+        music.SetActive(true);
+    }
+
+    public void OnClickButtonSound()
+    {
+        GameController.soundON_ = !GameController.soundON_;
+    }
+
+    public void OnClickButtonMusic()
+    {
+        GameController.musicON_ = !GameController.musicON_;
+    }
+
+    #endregion
+
+    #region Set Language
+
     private void SetLanguage()
     {
         if (selectEN_)
         {
-            ChangeLanguageSettings(language_en_menuSettings_);
-            ChangeLanguageStart(language_en_menuStart_);
-            ChangeButton(buttonActiveEN_);
+            CLanguagehangeButton(buttonActiveEN_);
+            for(int i = 0; i < language_en_.Length; i++)
+            {
+                language_en_[i].SetActive(true);
+            }
+
+            for (int i = 0; i < language_pt_.Length; i++)
+            {
+                language_pt_[i].SetActive(false);
+            }
         }
 
 
         if (selectPT_)
         {
-            ChangeLanguageSettings(language_pt_menuSettings_);
-            ChangeLanguageStart(language_pt_menuStart_);
-            ChangeButton(buttonActivePT_);
+            CLanguagehangeButton(buttonActivePT_);
+            for (int i = 0; i < language_pt_.Length; i++)
+            {
+                language_pt_[i].SetActive(true);
+            }
+
+            for (int i = 0; i < language_en_.Length; i++)
+            {
+                language_en_[i].SetActive(false);
+            }
         }
     }
-    private void ChangeButton(GameObject button)
+    private void CLanguagehangeButton(GameObject button)
     {
         buttonActiveEN_.SetActive(false);
         buttonActivePT_.SetActive(false);
@@ -76,37 +150,17 @@ public class MenuController : MonoBehaviour
         button.SetActive(true);
     }
 
-    private void ChangeLanguageSettings(GameObject language)
-    {
-        language_en_menuSettings_.SetActive(false);
-        language_pt_menuSettings_.SetActive(false);
-
-        language.SetActive(true);
-    }
-
-    private void ChangeLanguageStart(GameObject language)
-    {
-        language_en_menuStart_.SetActive(false);
-        language_pt_menuStart_.SetActive(false);
-
-        language.SetActive(true);
-    }
-
-    public void EN()
+    public void OnClickButtonEN()
     {
         selectEN_ = true;
         selectPT_ = false;
     }
 
-    public void PT()
+    public void OnClickButtonPT()
     {
         selectPT_ = true;
         selectEN_ = false;
     }
 
-    public void PlayAudioClip()
-    {
-        audioSource_.clip = audioClip_;
-        audioSource_.Play();
-    }
+    #endregion
 }
